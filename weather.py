6a1584cdce12994dbf49c7ffbe5bbf5c8580019c
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-01-16 05:09:58 +0530

    adding weather server

---
+++ weather.py
@@ -0,0 +1,11 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp =FastMCP("Weather")
+
+@mcp.tool()
+def get_weather(city:str)->str:
+    """Get the weather for a given city"""
+    return f"The weather in {city} is rainy"
+    
+if __name__=="__main__":
+    mcp.run(transport="streamable-http")
